d76ee40d2c56b2e2761e20558ba80dd01ea16e26
Ino Van Winckel <inovanwinckel@hotmail.com>
2016-10-28 12:34:16 +0200

    Concepts page added

--- resources/views/concepts/index.blade.php
+++ resources/views/concepts/index.blade.php
@@ -1,5 +1,16 @@
 @extends('master')
 
 @section('content')
-<p>concepts</p>
+<div class="row concept-content">
+    <div class="col-lg-12">
+        <div class="concept c1 col-lg-4">CONCEPT1</div>
+        <div class="concept c2 col-lg-4">CONCEPT2</div>
+        <div class="concept c3 col-lg-4">CONCEPT3</div>
+    </div>
+    <div class="col-lg-12">
+        <div class="concept c1 col-lg-4">CONCEPT1</div>
+        <div class="concept c2 col-lg-4">CONCEPT2</div>
+        <div class="concept c3 col-lg-4">CONCEPT3</div>
+    </div>
+</div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,12 +6,8 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-<<<<<<< HEAD
-	<div class="container">
-=======
     <div class="container">
         @include('partials.header')
->>>>>>> origin/dev
         @yield('content')
     </div>
 </body>
