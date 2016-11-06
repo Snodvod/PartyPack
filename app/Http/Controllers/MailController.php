cf2d1f5f2e0641d431812ce82724a8ccd8db9f06
Pjai1 <ulti40@hotmail.com>
2016-11-06 15:28:31 +0100

    Email Added

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function sendInvitation()
+    {
+    	$emailUser = 'jonas.vaneeckhout@student.kdg.be';
+    	$nameUser = 'Jos';
+    	$data = ['email' => $emailUser, 'name' => $nameUser];
+
+    	Mail::send('email.invitation', $data, function($message) use ($data) 
+    	{
+    		$emailUser = $data['email'];
+            $nameUser = $data['name'];
+            $message->to($emailUser, $nameUser)->subject('Hello Party Invite');
+    	});
+
+    	return 'success sending invite';
+    }
+
+
+ //    public function emailWinner(Request $request) {
+ //        $userId = Auth::id();
+ //        $winningUser = DB::table('users')->select('email', 'name')->where('id', $userId)->get();
+ //        $emailUser = $winningUser[0]->email;
+ //        $nameUser = $winningUser[0]->name;
+ //        $data = ['name' => $nameUser, 'email' => $emailUser, 'userId' => $userId];
+ //        Mail::send('email.winner', $data, function($message) use ($data) {
+ //            $emailUser = $data['email'];
+ //            $nameUser = $data['name'];
+ //            $message->to($emailUser, $nameUser)->subject('Congratulations, you have won!');
+ //        });
+ //    }
+	// }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::resource('artists', 'UserController');
 Route::resource('concepts', 'PartyController');
 Route::get('concepts/{id}/comments', 'PartyController@showComments');
 Route::resource('comments', 'CommentController');
+Route::get('email', 'MailController@sendInvitation');
 
 Route::get('/', 'HomeController@index')->name('home');
 
