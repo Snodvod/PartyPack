c05bbcc0cbd1409f0f7b58e81e5b320fd87477bf
Pjai1 <ulti40@hotmail.com>
2017-08-21 18:27:04 +0200

    whoops

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <div class="row content">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading" style="font-size: 20px;font-weight: bold;">Register</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
